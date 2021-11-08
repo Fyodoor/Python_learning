e84714b0e522a42036f1961fce8b82c6b4ffd5ce
Fyodoor <91294516+Fyodoor@users.noreply.github.com>
2021-11-08 21:20:59 +0200

    Add files via upload

--- диплом/diplom_school.py
+++ диплом/diplom_school.py
@@ -18,6 +18,12 @@ class FileProvider:
         file = open(path, "w")
         data = file.writelines(data)
         file.close
+    
+    def write_from_scratch(self,path,data):
+        file = open(path, "w")
+        data = file.write(data)
+        file.close
+
         
     def clear(self, path):
         file = open(path, "w")
@@ -27,40 +33,23 @@ class FileProvider:
     def exists(self, path):
         return os.path.exists(path)
 
-class School:
-    def __init__(self, number_school, adress_school):
-        self.number_school = number_school
-        self.adress_school = adress_school
+class StudentsStorage(FileProvider):
+    def __init__(self , students):
         
-
-        
-
-
-class Students:
-    def __init__(self, first_name_student, last_name_student, age_student,
-                 number_class):
-        self.first_name_student = first_name_student
-        self.last_name_student = last_name_student
-        self.age_student = age_student
-        self.number_class = number_class        
-    
-class StudentsStorage:
-    def __init__(self):
-        
-        self.file_name = "students.json"
+        self.file_name = students
         
     def get_all(self):
-        if not file_provider.exists(self.file_name):
+        if not  super().exists(self.file_name):
             students = []
             self.safe_students(students)
             
-        data = file_provider.get(self.file_name)
+        data = super().get(self.file_name)
         students = jsonpickle.decode(data)
         return students
     
     def safe_students(self, students):
         json_data = jsonpickle.encode(students)
-        file_provider.writelines(self.file_name, json_data)
+        super().writelines(self.file_name, json_data)
             
     def add(self, student):
         students = self.get_all()
@@ -71,96 +60,196 @@ class StudentsStorage:
         students = self.get_all()
         students.pop(index)
         self.safe_students(students)
-        
+    
+class Student:
+    def __init__(self, first_name_student, last_name_student, age_student,
+                 number_class):
+        self.first_name_student = first_name_student
+        self.last_name_student = last_name_student
+        self.age_student = age_student
+        self.number_class = number_class   
 
-def print_students():
-    first_name_student = "Имя"
-    last_name_student = "Фамилия"
-    age_student = "Возраст"
-    number_class = "Номер класса"
+class School:
+    def __init__(self , name , adress) :
+        self.name = name
+        self.adress = adress
     
-    
-    print(f'{first_name_student:15}{last_name_student:15}{age_student:15}{number_class:15}')
-    
-    students = studentsStorage.get_all()
-    for i in range(len(students)):
-        print(f'{i + 1}.{students[i].first_name_student:15}{students[i].last_name_student:15}{students[i].age_student:15}{students[i].number_class:15}')
 
+class SchoolHandler:
 
-def add_new_student():
-    while True:
-        first_name_student = input("введите имя ученика: ")
-        last_name_student = input("введите фамилию ученика: ")
-        age_student = input("введите возраст ученика: ")   
-        number_class = input("введите класс ученика: ")
-        new_student = Students(first_name_student, last_name_student, age_student,
-                     number_class)
-        studentsStorage.add(new_student)
-        
-        print("Если хотите добавить ещё ученика, отправьте 1")
-        user_answer = get_user_answer()
-        if not user_answer == 1:
-            break
-        
-def get_user_answer():
-    user_input = input()
-    
-    while not user_input.isdigit():
-        print("Введите число")
-        user_input = input()
-    
-    return int(user_input)
-    
-def remove_student():
-    students = studentsStorage.get_all()
-    while True:
-        print("Выберите номер ученика которого надо удалить.")
-        
-        print_students()
-        user_answer = get_user_answer()        
-        if user_answer < 1 or user_answer > len(students):
-            continue
+    def __init__(self , school , students) :
+        self.school = school
+        self.student_handler = StudentHandler(students)
+        self.file_provider = FileProvider()
+
+    def change_name(self):
+        new_name = input('Введите новое название : ')
+        school_data = jsonpickle.decode(self.file_provider.get(self.school))
+        school_data.name = new_name
+        school_data_json = jsonpickle.encode(school_data)
+        self.file_provider.clear(self.school)
+        self.file_provider.write_from_scratch(self.school,school_data_json)
+
+    def change_adress(self):
+        new_adress = input('Введите новый адресс : ')
+        school_data = jsonpickle.decode(self.file_provider.get(self.school))
+        school_data.adress = new_adress
+        school_data_json = jsonpickle.encode(school_data)
+        self.file_provider.clear(self.school)
+        self.file_provider.write_from_scratch(self.school,school_data_json)
+
+    def print_name(self):
+        school_data = jsonpickle.decode(self.file_provider.get(self.school))
+        print(f'{school_data.name}')
+
+    def print_adress(self):
+        school_data = jsonpickle.decode(self.file_provider.get(self.school))
+        print(f'{school_data.adress}')
+
+    def school_handler_main(self):
+        while True:
+            print('Меню Школы')
+            print('Показать название школы (1)')
+            print('Показать показать адрес школы (2)')
+            print('Изменить адрес (3)')
+            print('Изменить название (4)')
+            print('Отобразить меню учеников (5)')
+            print('Закончить (Любой другой символ)')
+
+            user_input = input()
             
-        studentsStorage.remove(user_answer - 1)
-        
-        print(f"Ученик под номером {user_answer} успешно удален")
-        
-        print("Если хотите удалить ещё ученика, отправьте 1")
-        user_answer = get_user_answer()
-        if not user_answer == 1:
-            break        
+            
+            if user_input == "1":
+                self.print_name() 
+            elif user_input == "2":
+                self.print_adress()    
+            elif user_input == "3":
+                self.change_adress()    
+            elif user_input == "4":
+                self.change_name()    
+            elif user_input == "5":
+                self.student_handler.student_handler_main()    
+            else:
+                break
         
 
+class StudentHandler:
 
+    def __init__(self , students):
+        # self.file_provider = FileProvider()
+        self.studentsStorage = StudentsStorage(students)
     
-file_provider = FileProvider()
-studentsStorage = StudentsStorage()
-
-
-
-while True:
-    
-    students = studentsStorage.get_all()
-    count_students = len(students)   
-    
-    
-    print("Показать список учащихся? Нажмите 1")
-    user_answer = int(input())
-    if user_answer == 1:
-        print_students()    
+    def add_new_student(self):
         
-    print("Хотите добавить нового ученика? Для этого отправтье 1")
-    user_answer = int(input())
-    if user_answer == 1:
-        add_new_student()    
-    
-    print("Хотите удалить ученика? Для этого отправтье 1")
-    user_answer = int(input())
-    if user_answer == 1:
-        remove_student()
+        print("Введите Имя, Фамилию, Возраст, Номер класса")
+        first_name_student = validate_word(input("введите имя ученика: ") , 2 , 20)
+        last_name_student = validate_word(input("введите фамилию ученика: ") , 2 , 20)
+        age_student = validate_integer(input("введите возраст ученика: ") , 6 , 20)   
+        number_class = validate_integer(input("введите класс ученика: ") , 1 , 12)
+        new_student = Student(first_name_student, last_name_student, age_student,
+                    number_class)
+        self.studentsStorage.add(new_student)
+
+    def print_students(self):
+        first_name_student = "Имя"
+        last_name_student = "Фамилия"
+        age_student = "Возраст"
+        number_class = "Номер класса"
         
-    print("Вернуться к меню? Для этого отправтье 1")
-    user_answer = int(input())
+        
+        print(f'{first_name_student:15}{last_name_student:15}{age_student:15}{number_class:15}')
+        
+        students = self.studentsStorage.get_all()
+        for i in range(len(students)):
+            print(f'{i + 1}.{students[i].first_name_student:15}{students[i].last_name_student:15}{students[i].age_student:15}{students[i].number_class:15}')
+
+        
+    def remove_student(self):
+        students = self.studentsStorage.get_all()
+        while True:
+            self.print_students()
+            print("Выберите номер ученика которого надо удалить.")
+            user_answer = int(input())
+            
+            if user_answer < 1 or user_answer > len(students):
+                continue
+                
+            self.studentsStorage.remove(user_answer - 1)
+            
+            print(f"{user_answer} успешно удален")
+            break
+
+    def student_handler_main(self):
+        while True:
+        
+            print('Меню студентов')
+            print('Показать список (1)')
+            print('Добавить (2)')
+            print('Удалить (3)')
+            print('Закончить (Любой другой символ)')
+            user_input = input()
+            
+            
+            if user_input == "1":
+                self.print_students() 
+            elif user_input == "2":
+                self.add_new_student()    
+            elif user_input == "3":
+                self.remove_student()    
+            else:
+                break
+
+
+def validate_word(value , min_length , max_length):
+
+    value_arr = value.split()
     
-    if user_answer != 1:
-        break    
+    # проверка на символы
+    # проверка на числа
+    if not value.isalpha():
+            value = input('\nПоле не может содержать символы и числа , введите еще раз : ')
+            validate_word(value , min_length , max_length)
+        
+    if value.__len__() < min_length or value.__len__() > max_length:
+            value = input(f'\nПоле не может быть длинее {max_length} и короче {min_length} символов, введите еще раз : ')
+            validate_word(value , min_length , max_length)
+    
+    if not value.istitle():
+        value = input('\nПоле должно начинаться с заглавной буквы и остальные буквы должны быть в нижнем регистре , введите еще раз : ')
+        validate_word(value , min_length , max_length)
+        
+    return value
+
+    
+
+def validate_integer(value , min_d , max_d):
+
+    while tryParse(value) == False:
+      
+        value = input('\nРазрешается только число : ')
+        validate_integer(value , min_d , max_d)
+
+    if int(value) < min_d or int(value) > max_d:
+        value = input(f'\nРазрешается ввести не меньше {min_d} и не больше {max_d} : ')
+        validate_integer(value , min_d , max_d)
+
+    return value
+
+def tryParse(value):
+    try:
+        return True , int(value)
+    except ValueError:
+        return False
+
+jsonpickle.set_encoder_options("json", indent=4,
+                               separators=(",", ": "),
+                               ensure_ascii=False)
+
+
+school_handler = SchoolHandler('school.json' , 'students.json')
+
+
+
+
+if __name__ == '__main__':
+    school_handler.school_handler_main()
